c73575c5e39ff3e2c3e826cd93e85da5e16323f5
José Gabriel Romero Atienza <josega1983@gmail.com>
2021-04-26 22:01:48 +0100

    First steps in JS

---
+++ web-ejemplo/script.js
@@ -0,0 +1,23 @@
+const list =[]
+const anotherList = list.concat(157)
+
+const persona = {
+    name: 'Jose',
+    twitter: '@jgratienza',
+    age: 38,
+    isDeveloper: true
+
+}
+
+const sumar = (a, b) => {
+    console.log (a)
+    console.log (b)
+    return a + b
+}
+
+function restar (a, b){
+    return a - b
+}
+
+console.log(sumar (2,2))
+console.log(restar (2,2))
