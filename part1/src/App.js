3589edb10e0db141fc390a7d9683dd3f75425319
José Gabriel Romero Atienza <josega1983@gmail.com>
2021-04-26 23:44:58 +0100

    Installation and first steps with react

---
+++ part1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Mensaje from './Mensaje.js'
+
+const App = () => {
+   return (
+    <div className="App">
+      <Mensaje />
+      <Mensaje />
+      <Mensaje />
+    </div>
+  );
+}
+
+export default App;
---
+++ part1/src/Mensaje.js
@@ -0,0 +1,5 @@
+const Mensaje = () => {
+    return <h1>Hola funiona desde modulo</h1>
+}
+
+export default Mensaje
